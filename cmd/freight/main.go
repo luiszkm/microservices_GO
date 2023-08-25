6ce3f261c302962fcd3189ecf099ba0648df8478
luis <luiszkm@gmail.com>
2023-08-25 11:10:16 -0300

    feat: add prometheus

--- cmd/freight/main.go
+++ cmd/freight/main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 
 	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	_ "github.com/go-sql-driver/mysql"
@@ -12,7 +13,40 @@ import (
 	"github.com/luiszkm/microservices_GO/internal/Domain/freight/entity"
 	"github.com/luiszkm/microservices_GO/internal/Infra/repository"
 	"github.com/luiszkm/microservices_GO/pkg/kafka"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var (
+	routesCreated = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "routes_created_total",
+			Help: "Total number of created routes",
+		},
+	)
+
+	routesStarted = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "routes_started_total",
+			Help: "Total number of started routes",
+		},
+		[]string{"status"},
+	)
+
+	errorsTotal = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "errors_total",
+			Help: "Total number of errors",
+		},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(routesStarted)
+	prometheus.MustRegister(errorsTotal)
+	prometheus.MustRegister(routesCreated)
+}
 
 func main() {
 	msgChan := make(chan *ckafka.Message)
@@ -26,6 +60,11 @@ func main() {
 	}
 	defer db.Close()
 	CreateTableRoutesMysql(db)
+	http.Handle("/metrics", promhttp.Handler())
+	go func() {
+		http.ListenAndServe(":8080", nil)
+	}()
+	go http.ListenAndServe(":8081", nil)
 	repository := repository.NewRouterRepositoryMysql(db)
 	freight := entity.NewFreight(10)
 
@@ -38,22 +77,36 @@ func main() {
 
 		switch input.Event {
 		case "RouteCreated":
-			output, err := createRouteUseCase.Execute(input)
+			_, err := createRouteUseCase.Execute(input)
 			if err != nil {
 				fmt.Println(err)
+				errorsTotal.Inc()
+			} else {
+				routesCreated.Inc()
 			}
-			fmt.Println(output)
-		case "RouteStarted", "RouteFinished":
+		case "RouteStarted":
+
 			input := routeusecase.ChangeStatusInput{}
 			json.Unmarshal(msg.Value, &input)
-			output, err := changeRouteStatusUseCase.Execute(input)
+			_, err := changeRouteStatusUseCase.Execute(input)
 			if err != nil {
 				fmt.Println(err)
+				errorsTotal.Inc()
+			} else {
+				routesStarted.WithLabelValues("started").Inc()
 			}
-			fmt.Println(output)
+		case "RouteFinished":
+			input := routeusecase.ChangeStatusInput{}
+			json.Unmarshal(msg.Value, &input)
+			_, err := changeRouteStatusUseCase.Execute(input)
+			if err != nil {
+				fmt.Println(err)
+				errorsTotal.Inc()
+			} else {
+				routesStarted.WithLabelValues("finished").Inc()
+			}
 		}
 	}
-
 }
 
 func CreateTableRoutesMysql(db *sql.DB) {
@@ -76,5 +129,4 @@ func CreateTableRoutesMysql(db *sql.DB) {
 	}
 
 	fmt.Println("Table created successfully")
-
 }
